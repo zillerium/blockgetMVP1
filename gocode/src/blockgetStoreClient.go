68a59ddd379076b7ceaf29c8fadc96171729c5c1
Ubuntu <ubuntu@ip-172-31-40-250.ec2.internal>
2019-08-25 18:06:19 +0000

    client

--- gocode/src/blockgetStoreClient.go
+++ gocode/src/blockgetStoreClient.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-//	"bytes"
+	"bytes"
 	"context"
 //	"encoding/json"
 	"fmt"
-//	"io/ioutil"
+	"io/ioutil"
 	"log"
 //	"net/http"
 //	"net/url"
@@ -23,7 +23,9 @@ import (
 type server struct{}
 
 type StoreData struct {
-        Account string `json:"account"`
+        ID int32 `json:"id"`
+	Parent string `json:"parent"`
+	Account string `json:"account"`
         Content     string `json:"content"`
         CID     string `json:"cid"`
 }
@@ -48,30 +50,45 @@ func handleRequests() {
 
 }
 
-func clientStreaming(c storepb.StoreServiceClient) {
+func buildRequest(s []string) []*storepb.LongStoreRequest {
+//	ss:= []storepb.LongStoreRequest
+// int32 i =0
+//stringv := s[0]
+//	for i := 0; i < len(s); i++ {
+        //              println(i, apps[i])
+//	ss := []*storepb.LongStoreRequest{
+/*		&storepb.LongStoreRequest {
+                        Msg: &storepb.StoreMessage{
+                                Content: stringv,
+                                Account:  "trevor3",
+                                Parent: "parentrec",
+                                Id: 0,
+                        },
+                        }, 
+		}
+//	}
+	return ss
+*/
+ss := []*storepb.LongStoreRequest{}
+for i,str:=range s {
+  ss=append(ss,&storepb.LongStoreRequest {
+                  Msg: &storepb.StoreMessage{
+                            Content: str,
+                            Account:  "trevor3",
+                            Parent: "parentrec",
+                            Id: int32(i),
+                    }})
+}
+return ss
+
+
+}
+
+func clientStreaming(c storepb.StoreServiceClient, s []string) {
 
 	fmt.Println("starting client")
 
-	requests := []*storepb.LongStoreRequest{
-		&storepb.LongStoreRequest{
-                	Msg: &storepb.StoreMessage{
-                        	Content: "message1",
-                        	Account:  "trevor3",
-                	},
-		},	
-     		 &storepb.LongStoreRequest{
-                        Msg: &storepb.StoreMessage{
-                                Content: "message2",
-                                Account:  "trevor3",
-                        },
-                }, 
-		&storepb.LongStoreRequest{
-                        Msg: &storepb.StoreMessage{
-                                Content: "message3",
-                                Account:  "trevor3",
-                        },
-                },
-	}
+	requests := buildRequest(s)
 
 	stream, err := c.LongStore(context.Background())	
 	if err!=nil {
@@ -85,16 +102,54 @@ func clientStreaming(c storepb.StoreServiceClient) {
 	res, err := stream.CloseAndRecv()
     	if err!=nil {
                 log.Fatalf("error client stream %v", err)
+                log.Fatalf("error client stream %v", res)
         }
-	fmt.Printf("long store response %v", res)
+//	fmt.Printf("long store response %v", res)
 
 
+}
+
+func splitString(s string, n int) []string {
+    sub := ""
+    subs := []string{}
+
+    runes := bytes.Runes([]byte(s))
+    l := len(runes)
+    for i, r := range runes {
+        sub = sub + string(r)
+        if (i + 1) % n == 0 {
+            subs = append(subs, sub)
+            sub = ""
+        } else if (i + 1) == l {
+            subs = append(subs, sub)
+        }
+    }
+
+    return subs
 }
 
 func storeBTFS() {
 //	content := "This is a test message from, trevor"
 
-        cc, err := grpc.Dial("54.92.219.108:50052", grpc.WithInsecure())
+	dat, err := ioutil.ReadFile("sample.pdf")
+    
+	if err!=nil {
+                log.Fatalf("error opening file %v", err)
+        }
+	l:=len(string(dat))
+	blocksize := 8192
+	segments := l/blocksize
+	segments++
+
+	subs:=splitString(string(dat),blocksize)
+	
+	fmt.Print(len(string(dat)))
+	fmt.Print(" segments %d\n ", segments)
+fmt.Print(" len array subs %d\n ", len(subs))
+
+
+ 	
+	cc, err := grpc.Dial("54.92.219.108:50052", grpc.WithInsecure())
 
         if err != nil {
                 log.Fatalf("could not connect: %w", err)
@@ -104,7 +159,7 @@ func storeBTFS() {
 
         c := storepb.NewStoreServiceClient(cc)
 
-	clientStreaming(c)
+	clientStreaming(c, subs)
 
         log.Printf("created client: %f", c)
 
